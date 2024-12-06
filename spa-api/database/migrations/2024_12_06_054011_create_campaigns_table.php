e03759778f01cae967030586c17c15fb6d328ede
jmsfabiano <jmsfabiano@gmail.com>
2024-12-06 03:13:04 -0300

    laravel aplication

---
+++ spa-api/database/migrations/2024_12_06_054011_create_campaigns_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('city_group_id')->constrained('city_groups')->cascadeOnDelete();
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+        });        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaigns');
+    }
+};
---
+++ spa-api/database/migrations/2024_12_06_054011_create_discounts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('campaign_id')->constrained()->cascadeOnDelete();
+            $table->decimal('value_discount', 10, 2)->nullable();
+            $table->decimal('percentage_discount', 5, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discounts');
+    }
+};
